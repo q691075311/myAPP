0f42c301981e82c0abc799d319bdc7a54c5602aa
陶博 <691075311@qq.com>
2017-10-16 15:13:51 +0800

    启动图

---
+++ RYFrame/Classes/Controller/Launch/LaunchController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchController.h
+//  RYFrame
+//
+//  Created by 陶博 on 2017/10/16.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import "RYBaseController.h"
+
+@interface LaunchController : RYBaseController
+
+@end
---
+++ RYFrame/Classes/Controller/Launch/LaunchController.m
@@ -0,0 +1,37 @@
+//
+//  LaunchController.m
+//  RYFrame
+//
+//  Created by 陶博 on 2017/10/16.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import "LaunchController.h"
+
+@interface LaunchController ()
+
+@end
+
+@implementation LaunchController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
