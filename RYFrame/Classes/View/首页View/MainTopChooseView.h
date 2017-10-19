fdee294f0d445d97e3268d492f67bc1f85fc3af0
陶博 <691075311@qq.com>
2017-10-19 17:31:18 +0800

    tianjiao

---
+++ RYFrame/Classes/View/首页View/MainTopChooseView.h
@@ -0,0 +1,15 @@
+//
+//  MainTopChooseView.h
+//  RYFrame
+//
+//  Created by 陶博 on 2017/10/19.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTopChooseView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame;
+
+@end
---
+++ RYFrame/Classes/View/首页View/MainTopChooseView.m
@@ -0,0 +1,34 @@
+//
+//  MainTopChooseView.m
+//  RYFrame
+//
+//  Created by 陶博 on 2017/10/19.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import "MainTopChooseView.h"
+
+@implementation MainTopChooseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addTopViewSubview];
+    }
+    return self;
+}
+
+- (void)addTopViewSubview{
+    
+}
+
+
+@end
